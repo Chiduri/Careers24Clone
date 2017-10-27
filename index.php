e3721b5f38cfd2773c6e6f375769da237fd02683
Chiduri <israel@jozicreations.co.za>
2017-10-27 18:53:46 +0200

    updated the file as requested

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Grocery</title>
+  </head>
+  <body>
+    <ul>
+      <li>bread</li>
+      <li>cheese</li>
+    </ul>
+
+  </body>
+</html>
